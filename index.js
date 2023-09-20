5828029ca4bfec01ed0bc9a6caa2ce274be30b65
Sabarinathan Arumugam <126689513+Sabari-Dev@users.noreply.github.com>
2023-09-20 16:03:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
 app.use("/api/s1/users", userRoutes);
 app.use("/api/s1/products", productRoutes);
